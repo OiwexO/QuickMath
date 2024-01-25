f2893abf4bb463f86158c9471eaf718786181bee
OiwexO <denyskostetskyi@gmail.com>
2024-01-25 16:15:10 +0200

    + architecture layers

--- app/src/main/java/com/iwex/quickmath/presentation/MainActivity.kt
+++ app/src/main/java/com/iwex/quickmath/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.iwex.quickmath
+package com.iwex.quickmath.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iwex.quickmath.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
